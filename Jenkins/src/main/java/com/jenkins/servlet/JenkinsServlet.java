3bf1f85723918a506e95ef67f26a143c5c5948cb
amdoramus <37803645+amdoramus@users.noreply.github.com>
2018-04-26 23:14:28 -0400

    Changed servlet text

--- Jenkins/src/main/java/com/jenkins/servlet/JenkinsServlet.java
+++ Jenkins/src/main/java/com/jenkins/servlet/JenkinsServlet.java
@@ -24,7 +24,7 @@ public class JenkinsServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.write("Mimikyu is the best!");
+		out.write("Dogs are cute! :3");
 	}
 
 	/**
